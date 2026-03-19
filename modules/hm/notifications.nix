ee2d401f32675a4fd8bfc24c89633e95b6524256
florianvazelle <ponythugflorian@gmail.com>
2026-03-19 19:07:45 +0100

    feat: add swaync notifications

--- modules/hm/notifications.nix
+++ modules/hm/notifications.nix
@@ -12,25 +12,68 @@ in {
       default = config.hydenix.hm.enable;
       description = "Enable notifications module";
     };
-  };
 
-  config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      dunst # notification daemon
-    ];
-
-    home.file = {
-      # # stateful file for themes
-      ".config/dunst/dunstrc" = {
-        source = "${pkgs.hyde}/Configs/.config/dunst/dunstrc";
-        force = true;
-        mutable = true;
+    dunst = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable dunst notification daemon";
       };
-      ".config/dunst/dunst.conf" = {
-        source = "${pkgs.hyde}/Configs/.config/dunst/dunst.conf";
-        force = true;
-        mutable = true;
+    };
+
+    swaync = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable SwayNC notification daemon";
       };
     };
   };
+
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    # Dunst (default)
+    (lib.mkIf cfg.dunst.enable {
+      home.packages = with pkgs; [
+        dunst # notification daemon
+      ];
+
+      home.file = {
+        ".config/dunst/dunstrc" = {
+          source = "${pkgs.hyde}/Configs/.config/dunst/dunstrc";
+          force = true;
+          mutable = true;
+        };
+        ".config/dunst/dunst.conf" = {
+          source = "${pkgs.hyde}/Configs/.config/dunst/dunst.conf";
+          force = true;
+          mutable = true;
+        };
+      };
+    })
+
+    # SwayNC (optional alternative)
+    (lib.mkIf cfg.swaync.enable {
+      home.packages = with pkgs; [
+        swaynotificationcenter
+      ];
+
+      home.file = {
+        ".config/swaync/config.json" = {
+          source = "${pkgs.hyde}/Configs/.config/swaync/config.json";
+          force = true;
+          mutable = true;
+        };
+        ".config/swaync/style.css" = {
+          source = "${pkgs.hyde}/Configs/.config/swaync/style.css";
+          force = true;
+          mutable = true;
+        };
+        ".config/swaync/user-style.css" = {
+          source = "${pkgs.hyde}/Configs/.config/swaync/user-style.css";
+          force = true;
+          mutable = true;
+        };
+      };
+    })
+  ]);
 }
