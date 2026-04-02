484f75e00092f31b626b9c86f782e656b99c95cf
florianvazelle <florianvazelle@users.noreply.github.com>
2026-04-02 06:21:21 +0000

    deploy: cdf0da0e6a6fb26f5578236d333c50bbc38b9b35

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-770d0c2a.js',
+                path_to_root + 'searchindex-3de45732.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
