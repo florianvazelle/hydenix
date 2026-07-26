fe161c3060d99451bc17b4d9987484c604bdb5c8
florianvazelle <23208753+florianvazelle@users.noreply.github.com>
2026-07-26 01:46:12 +0000

    chore(deps): update flake.lock

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/61b7c44c4073f0b827768aff0049561b5110ea5a";
+    nixpkgs.url = "github:nixos/nixpkgs/e2587caef70cea85dd97d7daab492899902dbf5d";
 
     # Home Manager (for user specific configuration)
     home-manager.url = "github:nix-community/home-manager";
